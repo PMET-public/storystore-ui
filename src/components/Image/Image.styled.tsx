40cb716a5404b8d4a34a4ec4dca1912c01ecf98c
Carlos A. Cabrera <cacabrer@adobe.com>
2020-05-09 10:19:23 -0500

    fixed transparent png bg

--- src/components/Image/Image.styled.tsx
+++ src/components/Image/Image.styled.tsx
@@ -1,11 +1,10 @@
 import styled from 'styled-components'
 
 export const Root = styled.img<{ $loaded?: boolean; $vignette?: boolean }>`
-    background: rgba(204, 204, 204, 0.5);
     transition-property: opacity;
     transition-duration: 500ms;
     transition-timing-function: ease;
     opacity: ${({ $loaded }) => ($loaded ? 1 : 0.5)};
-    filter: ${({ $vignette }) => $vignette && `brightness(0.95)`};
     overflow: hidden;
+    ${({ $vignette }) => $vignette && `filter: brightness(0.95);`}
 `

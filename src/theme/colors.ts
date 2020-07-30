4472d95ae73d25e51a1a740f31fb3fb196dd1bfb
Carlos A. Cabrera <cacabrer@adobe.com>
2020-07-30 10:24:36 -0500

    clean up

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -23,49 +23,6 @@ const hexToRGBA = (h: string, alpha = 1) => {
     return 'rgb(' + +r + ',' + +g + ',' + +b + ',' + alpha / 100 + ')'
 }
 
-const hexToHSL = (H: string, lightness?: number) => {
-    // Convert hex to RGB first
-    let r: any = 0,
-        g: any = 0,
-        b: any = 0
-    if (H.length == 4) {
-        r = '0x' + H[1] + H[1]
-        g = '0x' + H[2] + H[2]
-        b = '0x' + H[3] + H[3]
-    } else if (H.length == 7) {
-        r = '0x' + H[1] + H[2]
-        g = '0x' + H[3] + H[4]
-        b = '0x' + H[5] + H[6]
-    }
-    // Then to HSL
-    r /= 255
-    g /= 255
-    b /= 255
-    const cmin = Math.min(r, g, b),
-        cmax = Math.max(r, g, b),
-        delta = cmax - cmin
-
-    let h = 0,
-        s = 0,
-        l = 0
-
-    if (delta == 0) h = 0
-    else if (cmax == r) h = ((g - b) / delta) % 6
-    else if (cmax == g) h = (b - r) / delta + 2
-    else h = (r - g) / delta + 4
-
-    h = Math.round(h * 60)
-
-    if (h < 0) h += 360
-
-    l = (cmax + cmin) / 2
-    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1))
-    s = +(s * 100).toFixed(1)
-    l = +(l * 100).toFixed(1)
-
-    return 'hsl(' + h + ',' + s + '%,' + (lightness ?? l) + '%)'
-}
-
 export const generateColorTheme = (colors: { [key: string]: string }) => {
     const result: { [key: string]: string } = {}
 
@@ -78,11 +35,6 @@ export const generateColorTheme = (colors: { [key: string]: string }) => {
         for (let i = 5; i < 100; i = i + 5) {
             result[color + i] = hexToRGBA(value, i)
         }
-    }
-
-    // Generate Gray Color
-    if (colors.onSurface) {
-        result.graySurface = hexToHSL(colors.onSurface, 95)
     }
 
     return result
@@ -114,6 +66,8 @@ export const defaultColors = {
         success: '#008b8b',
         onSuccess: '#ffffff',
     }),
+
+    graySurface: '#f3f3f3',
 }
 
 export type ThemeColors = typeof defaultColors

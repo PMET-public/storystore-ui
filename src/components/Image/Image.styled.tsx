c1cb5e62babd83da1d367a3d0db5589cd321739e
Carlos A. Cabrera <cacabrer@adobe.com>
2020-08-18 17:52:21 -0500

    faster

--- src/components/Image/Image.styled.tsx
+++ src/components/Image/Image.styled.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const Img = styled.img<{ $loaded?: boolean; $vignette?: boolean }>`
     transition-property: opacity;
-    transition-duration: 500ms;
+    transition-duration: 200ms;
     transition-timing-function: ease;
     opacity: ${({ $loaded }) => ($loaded ? 1 : 0.5)};
     overflow: hidden;
